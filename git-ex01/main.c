8ba3928e3a73bf9e2224e23a8e8133f953546292
miju <miju0307@gmail.com>
2021-02-23 19:41:37 +0900

    멀티 프로젝트 실행 예제

---
+++ git-ex01/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+
+	printf("멀티프로젝트 1\n\n");
+	return 0;
+}
---
+++ git-ex02/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+	
+	printf("멀티 프로젝트 2번 \n\n");
+	return 0;
+}
